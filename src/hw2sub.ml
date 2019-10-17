778ca1afaba7fd5e8be9177fb8aa65ce6920c964
Simmo Saan <simmo.saan@gmail.com>
2019-10-17 22:38:59 +0300

    Solve HW2 polynomial sub

---
+++ src/hw2sub.ml
@@ -0,0 +1,15 @@
+open Common
+
+let solve_sub_task () =
+    let p = read_int () in
+    let module F = Zn (struct let n = p end) in
+    let module PolF = Pol (F) in
+
+    let module PolFIO = IntPolIO (PolF) in
+    let a = PolFIO.read_pol () in
+    let b = PolFIO.read_pol () in
+
+    let c = PolF.(a + neg b) in
+    PolFIO.print_pol c
+
+let () = solve_sub_task ()
