92ff0b2351a4285fb457f477b4e4a915902f550b
Simmo Saan <simmo.saan@gmail.com>
2019-12-06 16:43:21 +0200

    Add List.init implementation for HackerRank

--- src/common.ml
+++ src/common.ml
@@ -1,3 +1,20 @@
+(* for HackerRank's suddenly outdated OCaml... *)
+(* module List =
+struct
+    include List
+
+    (* copied from OCaml 4.06 List.ml *)
+    let init len f =
+        let rec init_aux i n f =
+            if i >= n then
+                []
+            else
+                let r = f i in
+                r :: init_aux (i+1) n f
+        in
+        init_aux 0 len f
+end *)
+
 let rec repeat f a n x = match n with
     | 0 -> a
     | n -> f x (repeat f a (n - 1) x)
